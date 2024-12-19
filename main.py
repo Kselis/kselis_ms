88ac985aa63d1cdcb536a2e95a6b9226ed044fa0
Kselis <kselis.institute@gmail.com>
2024-12-19 13:15:57 +0530

    new files added

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
 import argparse as arg
 
-print("duckie")
+file_nme = "D:\\bqsl\\project_submissions\\loan_approval_dataset.csv"
+
+
+
+df = pd.read_csv(file_nme)
+
+
+print(df)
